60062639d42134c8e891f154375fec6ce2ebe4f8
Vasileios Georgitzikis <georgitzik@ceid.upatras.gr>
2011-10-11 23:15:47 +0300

    Initial Commit

---
+++ Hackerspace Status Notifier/Hackerspace_Status_NotifierAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Hackerspace_Status_NotifierAppDelegate.h
+//  Hackerspace Status Notifier
+//
+//  Created by Vasileios Georgitzikis on 11/10/11.
+//  Copyright 2011 Tzikis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Hackerspace_Status_NotifierAppDelegate : NSObject <NSApplicationDelegate> {
+	NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Hackerspace Status Notifier/Hackerspace_Status_NotifierAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  Hackerspace_Status_NotifierAppDelegate.m
+//  Hackerspace Status Notifier
+//
+//  Created by Vasileios Georgitzikis on 11/10/11.
+//  Copyright 2011 Tzikis. All rights reserved.
+//
+
+#import "Hackerspace_Status_NotifierAppDelegate.h"
+
+@implementation Hackerspace_Status_NotifierAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
